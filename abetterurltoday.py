f1139c00f6924121fcf85636b539085e98a97b1c
Sangwon Kim <kittyjls13@gmail.com>
2017-07-22 20:08:40 -0700

    Replaced id in shortened url with hash

--- abetterurltoday.py
+++ abetterurltoday.py
@@ -2,6 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 from flask import render_template, request, redirect, url_for, abort
 import time
+from hashids import Hashids
+hashids = Hashids()
 
 import sqlite3
 from flask import g
@@ -35,11 +37,17 @@ def shorten_url():
   cursor.fetchall()
   db.commit()
 
-  url_tail = url_for('redirect_to_shortened_url', id=cursor.lastrowid)
+  # generate hash with id
+  url_hash = hashids.encode(cursor.lastrowid)
+
+  url_tail = url_for('redirect_to_shortened_url', url_hash=url_hash)
   return 'http://' + request.host + url_tail
 
-@app.route('/<id>')
-def redirect_to_shortened_url(id=None):
+@app.route('/<url_hash>')
+def redirect_to_shortened_url(url_hash=None):
+  # decode hash to find id
+  id = hashids.decode(url_hash)[0] # decode() returns tuple
+
   # if id doesn't exist, return 404 error
   if not get_db().execute('SELECT EXISTS(SELECT id FROM urls WHERE id=?)', (id,)).fetchall()[0][0]:
     abort(404)
@@ -48,5 +56,5 @@ def redirect_to_shortened_url(id=None):
   original_url = get_db().execute(
     'SELECT original_url FROM urls WHERE id=?',
     (id,)
-  ).fetchall()[0][0] # fetchall() >> tuple inside array
+  ).fetchall()[0][0] # fetchall() returns tuple inside array
   return redirect(original_url)
